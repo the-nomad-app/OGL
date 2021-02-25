65df34f3829dbe5e8de511001756b49dee642f1b
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-02-25 13:16:44 -0700

    Search input and button added to TravelServices page.  searchLocation state created with function to setSearchLocation to input value when search button is clicked.

--- client/src/components/pages/travelservices/travelservices.js
+++ client/src/components/pages/travelservices/travelservices.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function TravelServices() {
+  const [searchLocation, setSearchLocation] = useState();
+
+  function setLocation() {
+    var location = document.querySelector(".search").value;
+    setSearchLocation(location);
+  }
+
   return (
     <div>
       <h1>Travel Services</h1>
+      <h5>Search for travel services here.</h5>
+      <input className="search" placeholder="Search Location"></input>
+      <button onClick={setLocation}>Search</button>
       <hr></hr>
-      <p>Travel Servies stuff goes here.</p>
+      <p>Travel Services stuff goes here.</p>
     </div>
   );
 }
