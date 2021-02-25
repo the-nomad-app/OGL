429bec000ea4411bae5f70825b0ec7490690a3ec
johndolac <johndolacjr@gmail.com>
2021-02-24 20:18:21 -0700

    hero to home

--- client/src/components/HomeSection.js
+++ client/src/components/HomeSection.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import '../App.css';
 // import { Button } from './Button';
-import './HeroSection.css';
+import './HomeSection.css';
 import './images/profile.jpeg/'
-function HeroSection() {
+function HomeSection() {
   return (
-    <div className='hero-container'>
+    <div className='home-container'>
       <embed type="image/jpg" src="profile.jpeg" width="300" height="200"></embed>
       <p>My name is <strong>Chance Ewell</strong>.</p>
       <br></br>
@@ -14,4 +14,4 @@ function HeroSection() {
   );
 }
 
-export default HeroSection;
+export default HomeSection;
