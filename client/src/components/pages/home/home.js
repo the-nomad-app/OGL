c315713dc34753684192eea7ce1b10ba2752d4cf
CHANCEEWELL <chance.shane.ewell@gmail.com>
2021-02-28 00:04:59 -0700

    added herosection css

--- client/src/components/pages/home/home.js
+++ client/src/components/pages/home/home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import '../../../App.css';
 import './home.css';
+import './HeroSection.css';
 // import HeroSection from '../../HeroSection';
 // import Footer from '../../Footer';
 
