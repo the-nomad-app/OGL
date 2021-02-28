c2ab82891b89a9453b079547750972564dc77531
CHANCEEWELL <chance.shane.ewell@gmail.com>
2021-02-27 23:16:06 -0700

    commit out section change

--- server.js
+++ server.js
@@ -4,60 +4,60 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
-const bodyParser = require('body-parser')
-const Pusher = require('pusher');
+// const bodyParser = require('body-parser')
+// const Pusher = require('pusher');
 
-// initialize pusher
-const pusher = new Pusher({
-    appId: "1162234",
-    key: "9fc95fa77ecdd821257a",
-    secret: "a695e432e435446474cc",
-    cluster: "us3",
-    useTLS: true
-});
+// // initialize pusher
+// const pusher = new Pusher({
+//     appId: "1162234",
+//     key: "9fc95fa77ecdd821257a",
+//     secret: "a695e432e435446474cc",
+//     cluster: "us3",
+//     useTLS: true
+// });
 
-pusher.trigger("my-channel", "my-event", {
-    message: "hello world"
-});
+// pusher.trigger("my-channel", "my-event", {
+//     message: "hello world"
+// });
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-// to Allow CORS
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    next();
-});
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// // to Allow CORS
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header(
+//         'Access-Control-Allow-Headers',
+//         'Origin, X-Requested-With, Content-Type, Accept'
+//     );
+//     next();
+// });
 
-app.post('/pusher/auth', (req, res) => {
-    let socketId = req.body.socket_id;
-    let channel = req.body.channel_name;
-    random_string = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
-    let presenceData = {
-        user_id: random_string,
-        user_info: {
-            username: '@' + random_string,
-        }
-    };
-    let auth = pusher.authenticate(socketId, channel, presenceData);
-    res.send(auth);
-});
+// app.post('/pusher/auth', (req, res) => {
+//     let socketId = req.body.socket_id;
+//     let channel = req.body.channel_name;
+//     random_string = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
+//     let presenceData = {
+//         user_id: random_string,
+//         user_info: {
+//             username: '@' + random_string,
+//         }
+//     };
+//     let auth = pusher.authenticate(socketId, channel, presenceData);
+//     res.send(auth);
+// });
 
-app.post('/update-location', (req, res) => {
-    // trigger a new post event via pusher
-    pusher.trigger('presence-channel', 'location-update', {
-        'username': req.body.username,
-        'location': req.body.location
-    })
-    res.json({ 'status': 200 });
-});
+// app.post('/update-location', (req, res) => {
+//     // trigger a new post event via pusher
+//     pusher.trigger('presence-channel', 'location-update', {
+//         'username': req.body.username,
+//         'location': req.body.location
+//     })
+//     res.json({ 'status': 200 });
+// });
 
-let port = 3128;
-app.listen(port);
-console.log('listening');
+// let port = 3128;
+// app.listen(port);
+// console.log('listening');
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
