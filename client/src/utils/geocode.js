c1a2728494870a60a30741a05f9c25232f6e396d
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-03-05 11:40:38 -0700

    Test API added part 2.

--- client/src/utils/geocode.js
+++ client/src/utils/geocode.js
@@ -7,18 +7,10 @@ export default {
       "https://geocode.search.hereapi.com/v1/geocode?q=" + search + "&apiKey=ajXCyBIYEY8xiKijsANkbShF3FN2FcIIAQ2jRUKtlyM"
     );
   },
-<<<<<<< Updated upstream
-  getGas: function() {
-    return axios.get(
-      "http://api.amp.active.com/camping/campgrounds?landmarkName=true&landmarkLat=37.84035&landmarkLong=-122.4888889&api_key=s4hjq23y6wxb9aaxsquxn2xs"
-    );
-  },
-=======
   getCampgrounds: function (lat, lng) {
     return axios.get(
       "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=campground&inputtype=textquery{PARAMETERS}&locationbias=circle:16000@" + lat + "," + lng + "&key=AIzaSyCpTnhn5_HjpXAv0eDGHXVJAR4qnWaaslw"
     );
   },
   // Google Places API documentation: https://developers.google.com/maps/documentation/places/web-service/search?hl=en_US
->>>>>>> Stashed changes
 };
