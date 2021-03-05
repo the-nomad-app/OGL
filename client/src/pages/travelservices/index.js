b014614212bc45d596aacfdfcdd2a3f9dbfcb13f
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-03-05 12:20:53 -0700

    getGas function added back in after accidentally deleting it.

--- client/src/pages/travelservices/index.js
+++ client/src/pages/travelservices/index.js
@@ -1,67 +1,69 @@
 import React from "react";
 import API from "../../utils/geocode";
 // import API from "../../../utils/API";
-import './travelservices.css';
-import GoogleMap from 'google-map-react';
+import "./travelservices.css";
+import GoogleMap from "google-map-react";
 // import axios from 'axios';
 // import Pusher from 'pusher-js';
 // import { ToastContainer, toast } from 'react-toastify';
 // import 'react-toastify/dist/ReactToastify.css';
 
 const mapStyles = {
-  width: '100%',
-  height: '100%'
-}
+  width: "100%",
+  height: "100%",
+};
 const markerStyle = {
-  height: '50px',
-  width: '50px',
-  marginTop: '-50px'
-}
+  height: "50px",
+  width: "50px",
+  marginTop: "-50px",
+};
 const imgStyle = {
-  height: '100%'
-}
+  height: "100%",
+};
 const Marker = ({ title }) => (
   <div style={markerStyle}>
-    <img style={imgStyle} src="https://res.cloudinary.com/og-tech/image/upload/s--OpSJXuvZ--/v1545236805/map-marker_hfipes.png" alt={title} />
+    <img
+      style={imgStyle}
+      src="https://res.cloudinary.com/og-tech/image/upload/s--OpSJXuvZ--/v1545236805/map-marker_hfipes.png"
+      alt={title}
+    />
     <h3>{title}</h3>
   </div>
 );
 
-function renderStations () {
-  API.getGas()
-  .then((res) => {
-    console.log("stations: ", res);
-  })
-  .catch((err) => console.log(err));
-}
-
 function TravelServices(props) {
-  renderStations();
-  console.log(props)
+  // API.getGas()
+  //   .then((res) => {
+  //     console.log("stations: ", res);
+  //   })
+  //   .catch((err) => console.log(err));
+
+  console.log(props);
   let locationMarkers = Object.keys(props.locations).map((username, id) => {
-      return (
-          <Marker
-              key={id}
-              title={`${username === props.current_user ? 'My location' : username + "'s location"}`}
-              lat={props.locations[`${username}`].lat}
-              lng={props.locations[`${username}`].lng}
-          >
-          </Marker>
-      );
+    return (
+      <Marker
+        key={id}
+        title={`${
+          username === props.current_user
+            ? "My location"
+            : username + "'s location"
+        }`}
+        lat={props.locations[`${username}`].lat}
+        lng={props.locations[`${username}`].lng}
+      ></Marker>
+    );
   });
 
-return (
-  <GoogleMap
-  style={mapStyles}
-  bootstrapURLKeys={{ key: 'AIzaSyAP93BAoflNBnB8aYrKvbJ8CwCCMRSa4fA' }}
-  center={props.center}
-  zoom={14}
->
-  {locationMarkers}
-</GoogleMap>
+  return (
+    <GoogleMap
+      style={mapStyles}
+      bootstrapURLKeys={{ key: "AIzaSyAP93BAoflNBnB8aYrKvbJ8CwCCMRSa4fA" }}
+      center={props.center}
+      zoom={14}
+    >
+      {locationMarkers}
+    </GoogleMap>
+  );
+}
 
-)}    
-
-export  {TravelServices};
-
-
+export { TravelServices };
--- client/src/utils/geocode.js
+++ client/src/utils/geocode.js
@@ -7,6 +7,11 @@ export default {
       "https://geocode.search.hereapi.com/v1/geocode?q=" + search + "&apiKey=ajXCyBIYEY8xiKijsANkbShF3FN2FcIIAQ2jRUKtlyM"
     );
   },
+  getGas: function() {
+    return axios.get(
+      "http://api.amp.active.com/camping/campgrounds?landmarkName=true&landmarkLat=37.84035&landmarkLong=-122.4888889&api_key=s4hjq23y6wxb9aaxsquxn2xs"
+    );
+  },
   getCampgrounds: function (lat, lng) {
     return axios.get(
       "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=campground&inputtype=textquery{PARAMETERS}&locationbias=circle:16000@" + lat + "," + lng + "&key=AIzaSyCpTnhn5_HjpXAv0eDGHXVJAR4qnWaaslw"
