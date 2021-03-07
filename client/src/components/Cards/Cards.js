ef5f93b99427e9c6a6a8e2e2ff4fba1256febaa4
johndolac <johndolacjr@gmail.com>
2021-03-06 21:42:01 -0700

    moved the login/register info to the top of the DOM

--- client/src/components/Cards/Cards.js
+++ client/src/components/Cards/Cards.js
@@ -1,16 +1,24 @@
 // import React from 'react';
 // import './Cards.css';
-// // import CardItem from '../CardItem';
+// import CardItem from '../CardItem';
 // import Login from '../Login.js';
 // import Register from '../Register';
 
 // function Cards() {
-//   return (
-//     <div className='cards'>
-//         <Login/>
-//         <Register/>
-//     </div>
-//   );
+//     return (
+//         <div className='cards'>
+//             <div className='cards__container'>
+//                 <div className='cards__wrapper'>
+//                     <ul className='cards__items'>
+//                         <CardItem>
+//                             <Login />
+//                             <Register />
+//                         </CardItem>
+//                     </ul>
+//                 </div>
+//             </div>
+//         </div>
+//     );
 // }
 
 // export default Cards;
--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -3,7 +3,6 @@ import Login from "../Login"
 import Register from "../Register"
 import "./home.css"
 // import Cards from "../../components/Cards/Cards"
-
 function Home() {
   const backgroundImgs = [ "./images/flatheadLake.jpg", "./images/hotSprings.jpg", "./images/theRonaArch.jpg", "./images/redFleet.jpg", "./images/mermaidPose.jpg", "./images/saltFlats.jpg",  ]
   // const styles={
@@ -13,17 +12,14 @@ function Home() {
   //     padding: "10%",
   //     zIndex: "1000",
   //     margin: "-20vh auto 0",
-      
   //   }
   // }
   return (
           <div className='home-container'>
-          <embed type="image/jpg" src= { backgroundImgs[Math.floor (Math.random() * backgroundImgs.length)] } width="100%" height="100%"></embed>
-        <div>
-        {/* style={styles.loginContainer} className='login-container' */}
+                    {/* style={styles.loginContainer} className='login-container' */}
           <Login />
           <Register />
-        </div>
+          <embed type="image/jpg" src= { backgroundImgs[Math.floor (Math.random() * backgroundImgs.length)] } width="100%" height="100%"></embed>
         </div>
       );
     }
