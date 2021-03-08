6afb6240b28b640cb32b9fd85e57b4cd1ce56688
johndolac <johndolacjr@gmail.com>
2021-03-07 20:53:51 -0700

    tweaked some front end

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,10 +1,11 @@
 import React from "react";
-import Login from "../Login"
-import Register from "../Register"
-import "./home.css"
+import Login from "../Login";
+import Register from "../Register";
+import "./home.css";
+
 // import Cards from "../../components/Cards/Cards"
 function Home() {
-  const backgroundImgs = [ "./images/flatheadLake.jpg", "./images/hotSprings.jpg", "./images/theRonaArch.jpg", "./images/redFleet.jpg", "./images/mermaidPose.jpg", "./images/saltFlats.jpg",  ]
+  const backgroundImgs = [ "./images/flatheadLake.jpg", "./images/cooleyGrandCanyon.jpg", "./images/cecretLake.jpg", "./images/redFleet.jpg", "./images/mermaidPose.jpg", "./images/saltFlats.jpg", "./images/antelopeIsland.jpg", "./images/arizonaRocks.jpg",  ]
   // const styles={
   //   loginContainer: {
   //     backgroundColor: "red",
@@ -17,9 +18,9 @@ function Home() {
   return (
     <div className='home-container'>
       {/* style={styles.loginContainer} className='login-container' */}
+      <embed type="image/jpg" src={backgroundImgs[Math.floor(Math.random() * backgroundImgs.length)]} width="100%" height="100%"></embed>
       <Login /> 
       <Register />
-      <embed type="image/jpg" src={backgroundImgs[Math.floor(Math.random() * backgroundImgs.length)]} width="100%" height="100%"></embed>
     </div>
   );
 }
